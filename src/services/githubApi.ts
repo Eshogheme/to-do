f0a64732e55b372fedf6765d71ec70c1284d7528
Kingsley Sabo Joseph <106545931+Eshogheme@users.noreply.github.com>
2024-03-29 07:35:42 +0100

    github API

---
+++ src/services/githubApi.ts
@@ -0,0 +1,30 @@
+import type { GitHubBranchResponse, GitHubInfoResponse, GitHubRepoResponse } from "../types/github";
+
+export const fetchGitHubInfo = async (): Promise<GitHubInfoResponse> => {
+  const username = "maciekt07";
+  const repo = "TodoApp";
+  const branch = "main";
+  try {
+    const [repoResponse, branchResponse] = await Promise.all([
+      fetch(`https://api.github.com/repos/${username}/${repo}`),
+      fetch(`https://api.github.com/repos/${username}/${repo}/branches/${branch}`),
+    ]);
+
+    if (repoResponse.ok && branchResponse.ok) {
+      const [repoData, branchData] = await Promise.all([
+        repoResponse.json() as Promise<GitHubRepoResponse>,
+        branchResponse.json() as Promise<GitHubBranchResponse>,
+      ]);
+
+      return {
+        repoData,
+        branchData,
+      };
+    } else {
+      throw new Error("Failed to fetch repository or branch information");
+    }
+  } catch (error) {
+    console.error(error);
+    return { repoData: {} as GitHubRepoResponse, branchData: {} as GitHubBranchResponse };
+  }
+};
