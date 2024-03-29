4baec5d5138adc06d847ab02abcba7781adf3f05
Kingsley Sabo Joseph <106545931+Eshogheme@users.noreply.github.com>
2024-03-29 07:27:35 +0100

    mui styles

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+export { displayGreeting } from "./displayGreeting";
+export { getRandomGreeting } from "./getRandomGreeting";
+export { getFontColor } from "./getFontColor";
+export { calculateDateDifference } from "./calculateTimeDiff";
+export { formatDate } from "./formatDate";
+export { getTaskCompletionText } from "./getTaskCompletionText";
+export { exportTasksToJson } from "./exportTasksToJson";
+export { timeAgo } from "./timeAgo";
+export { iOS } from "./iOS";
+export { saveQRCode } from "./saveQRCode";
